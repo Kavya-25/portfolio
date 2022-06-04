e48d6fc15ddbc15ebad540deef74c39ed7977ec0
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2022-06-04 10:19:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+AOS.init({
+    duration:1200,
+});
+let menuIcon=document.getElementById("menu-icon")
+let navUl=document.getElementById("nav-ul")
+let navMobile=document.getElementById("nav-mobile")
+
+menuIcon.addEventListener("click",function(){
+console.log("button clicked")
+navMobile.classList.toggle("show")
+})
+
+
